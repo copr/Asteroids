5b89608d98b84f19fa0537f1f316d950ddacb362
copr <martinkobersky@gmail.com>
2014-03-04 20:07:15 +0100

    Initial

---
+++ GameTest2/Asteroid.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Input;
+
+namespace GameTest2
+{
+    class Asteroid : INotifiable
+    {
+        public Asteroid(Canvas aCanvas)
+        {
+            mBitmapImage = new BitmapImage(new Uri("C:/Users/copr/Documents/Visual Studio 2013/Projects/Game/GameTest2/Image/asteroid2.png"));
+            mImage.Source = mBitmapImage;
+            mImage.Width = 50;
+            mImage.Height = 50;
+
+            mCanvas = aCanvas;
+            mCanvas.Children.Add(mImage);
+
+            Canvas.SetLeft(mImage, 200);
+            Canvas.SetTop(mImage, 200);
+        }
+        public void Notify(object[] args)
+        {
+            Canvas.SetLeft(mImage, 200);
+            Canvas.SetTop(mImage, 200);  
+        }
+
+        private BitmapImage mBitmapImage;
+        private Image mImage = new Image();
+        private Point mPosition;
+        private Canvas mCanvas;
+    }
+}
---
+++ GameTest2/INotifiable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameTest2
+{
+    interface INotifiable
+    {
+        void Notify(object[] args);
+    }
+}
