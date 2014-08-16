5b7a2bab3cca7a757194927e71d60560d7c6a8dc
Holali <bjakobecko@gmail.com>
2014-08-16 15:46:09 +0200

    Přidán soubor Engine/BaseViewModel.cs

---
+++ Engine/BaseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Engine
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler lHandler = PropertyChanged;
+            if (lHandler != null)
+            {
+                lHandler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
